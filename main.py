17d03e4bb56c691eb562af81906ee9ae38b45a93
saffronhamid <saffronhamid1234@gmail.com>
2024-10-25 21:00:57 +0200

    updated DI and DV

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-from textSummarizer.logging import logger
+from src.textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from src.textSummarizer.logging import logger
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -14,3 +15,13 @@ except Exception as e:
 
 
 
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_validation = DataValidationTrainingPipeline()
+   data_validation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
